c8024667f3e420dc03761181f3b05eecbbfa90e3
qianxuege <qianxuege15@gmail.com>
2025-09-06 14:28:46 -0400

    working on config

--- app/config/settings.py
+++ app/config/settings.py
@@ -4,8 +4,9 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your-jwt-secret")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/flaskapp")
     GOOGLE_CLIENT_SECRET_FILE = "client_secret.json"  # Google API credentials
     GOOGLE_SCOPES = ["https://www.googleapis.com/auth/calendar"]
+    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", "your-google-client-id")
+    GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", "your-google-client-secret")
 
 
